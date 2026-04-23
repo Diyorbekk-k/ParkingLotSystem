1572d11e9f54145682262814521a5e95fd6ada3c
Diyorbek <250275@newuu.uz>
2026-04-23 16:06:09 +0500

    initial project setup with JavaFX and MySQL

---
+++ src/main/java/CarParkingLot/main/Main.java
@@ -0,0 +1,17 @@
+package CarParkingLot.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Parking Lot System");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
